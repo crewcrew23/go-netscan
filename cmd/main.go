33f143b8296cbcf3fb8d63388969450afb0e680f
Vasiliy Iordanov <vasiliyiordanov23@gmail.com>
2025-07-07 00:40:21 +0300

    add urfave

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,63 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/crewcrew23/go-netscan/internal/util"
+	"github.com/urfave/cli/v3"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 )
 
 func main() {
-	device := util.SelectInterface()
-	fmt.Printf("you enter %s", device)
+	cmd := &cli.Command{
+		Name: "go-netscan",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "i",
+				Value:    "",
+				Usage:    "name of network interface",
+				Required: false,
+			},
 
-	conn, err := pcap.OpenLive(device, 65535, true, pcap.BlockForever)
+			&cli.BoolFlag{
+				Name:     "find-interfaces",
+				Usage:    "finds all available network interfaces",
+				Required: false,
+			},
+		},
+		Action: func(ctx context.Context, c *cli.Command) error {
+			netInterface := c.String("i")
+			findInterfaces := c.Bool("find-interfaces")
+
+			if !findInterfaces && netInterface == "" {
+				return errors.New("at least 1 flag is required")
+			}
+
+			if findInterfaces {
+				device := util.SelectInterface()
+				start(device)
+				return nil
+			}
+
+			start(netInterface)
+			return nil
+
+		},
+	}
+
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func start(netInterface string) {
+	conn, err := pcap.OpenLive(netInterface, 65535, true, pcap.BlockForever)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,5 +67,4 @@ func main() {
 	for packet := range packetSource.Packets() {
 		util.PrintPacketData(packet)
 	}
-
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.24.2
 
 require (
 	github.com/google/gopacket v1.1.19 // indirect
+	github.com/urfave/cli/v3 v3.3.8 // indirect
 	golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
 )
